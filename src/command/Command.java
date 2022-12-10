68d685279bc0736795c0ff496887ec2e885553b0
WalanGun <ryw36237@gmail.com>
2022-12-10 16:08:37 +0700

    add executedDateTime and its getter-setter

--- src/command/Command.java
+++ src/command/Command.java
@@ -1,16 +1,32 @@
 package command;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 import game.GameFactory;
 import user.UserFactory;
 
 public abstract class Command {
     GameFactory game;
     UserFactory user;
+    String executedDateTime;
 
     public Command(GameFactory game, UserFactory user) {
         this.game = game;
         this.user = user;
     }
 
-    abstract public void execute();
+    void setExecutedDateTime(String executedDetail) {
+        LocalDateTime dateTime = LocalDateTime.now();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy HH:mm:ss");
+        
+        String executedDateTime = "Executed on: " + dateTime.format(dateTimeFormatter);
+        this.executedDateTime = executedDetail + executedDateTime;
+    }
+
+    public String getExecutedDateTime() {
+        return executedDateTime;
+    }
+
+    abstract public void execute();    
 }
