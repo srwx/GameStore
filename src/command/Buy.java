63e4b99f589deb16cdbfcc39fc33a25a39a388e0
WalanGun <ryw36237@gmail.com>
2022-12-10 21:04:50 +0700

    init Buy command

--- src/command/Buy.java
+++ src/command/Buy.java
@@ -13,6 +13,16 @@ public class Buy extends Command {
     }
 
     public void execute() {
+        boolean paymentValidated = payment.validatePayment(game.getPrice());
+        if (paymentValidated) {
+            user.addGame(game);
+            System.out.println(game.getName() + " added to your library");
 
+            // set execution detail & datetime after execution complete
+            String executedDetail = "Successfully Buy " + game.getName() + "\n";
+            setExecutedDateTime(executedDetail);
+        } else {
+            System.out.println("Failed to buy " + game.getName());
+        }
     }
 }
