26029d74086df62c149e17e83e821c84cb810c7f
Sorawong L <Sorawong2543@gmail.com>
2022-12-10 22:03:09 +0700

    update  game command

--- src/command/Update.java
+++ src/command/Update.java
@@ -4,18 +4,35 @@ import game.GameFactory;
 import user.Publisher;
 import user.UserFactory;
 
+import java.util.HashMap;
+
 public class Update extends Command {
+    private HashMap<String, String> detail; // key: "name" | "price" | "description"
+
     public Update(GameFactory game, UserFactory user) {
         super(game, user);
     }
 
+    public HashMap<String, String> getDetail() {
+        return detail;
+    }
+
+    public void setDetail(HashMap<String, String> detail) {
+        this.detail = detail;
+    }
+
     public void execute() {
         if (user instanceof Publisher) {
-            // TODO: Update game logic
+            Publisher requetedPublisher = (Publisher) user;
+            boolean isSuccess = requetedPublisher.editGame(game, detail);
 
-            // set execution detail & datetime after execution complete
-            String executedDetail = "Update " + game.getName() + "information.\n";
-            setExecutedDateTime(executedDetail);
+            if(isSuccess) {
+                // set execution detail & datetime after execution complete
+                String executedDetail = "Update " + game.getName() + "information.\n";
+                setExecutedDateTime(executedDetail);
+            } else {
+                System.out.println("Update game failed.");
+            }
         } else {
             System.out.println("You have no permission to perform this action.");
         }
