358ca095fc807f7009254eba4f57e37845100382
WalanGun <ryw36237@gmail.com>
2022-12-11 16:46:16 +0700

    prevent failed command & reverse history order

--- src/command/CommandExecutor.java
+++ src/command/CommandExecutor.java
@@ -6,12 +6,13 @@ public class CommandExecutor {
     private ArrayList<Command> history = new ArrayList<Command>();
 
     public void executeCommand(Command command) {
-        history.add(command);
         command.execute();
+        if (command.executedDateTime != null)
+            history.add(command);
     }
 
     public void printHistoryCommand() {
-        for (int i = 0; i < history.size(); i++) {
+        for (int i = history.size()-1; i >= 0; i--) {
             Command command = history.get(i);
             System.out.println(command.getExecutedDateTime());
             System.out.println("==================================");
