933dc7c6dfb41c9b1cb2552c34c6b6806f8f8129
Sorawong L <Sorawong2543@gmail.com>
2022-12-11 15:42:55 +0700

    update information of dummy data

--- src/example/dummy_data/TempGame.java
+++ src/example/dummy_data/TempGame.java
@@ -5,13 +5,13 @@ import user.*;
 
 // This class used for generate dummy data of game
 public class TempGame {
-    public static Game temp1 = new Game("GameName1", "GameDescription1", 100, "GameCat1");
-    public static Game temp2 = new Game("GameName2", "GameDescription2", 200, "GameCat2");
-    public static Game temp3 = new Game("GameName3", "GameDescription3", 300, "GameCat3");
+    public static Game temp1 = new Game("Assassin's Creed Valhalla", "Become a legendary Viking on a quest for glory. Raid your enemies and build your political power.", 230, "Action");
+    public static Game temp2 = new Game("Divine Knockout (DKO)", "KO the gods in the world’s only 3rd-person platform fighter!", 180, "Action");
+    public static Game temp3 = new Game("Need for Speed™ Unbound", "Race to the top, definitely don’t flop. Outsmart the cops, and enter ultimate street race.", 470, "Racing");
 
-    public static Dlc dlc1 = new Dlc("DlcName1", "DlcDescription1", 50, temp1.getId());
-    public static Dlc dlc2 = new Dlc("DlcName2", "DlcDescription2", 60, temp1.getId());
-    public static Dlc dlc3 = new Dlc("DlcName3", "DlcDescription3", 70, temp3.getId());
+    public static Dlc dlc1 = new Dlc("Ragnarok Edition", "Ragnarok DLC for Assassin's Creed Valhalla", 80, temp1.getId());
+    public static Dlc dlc2 = new Dlc("Complete Edition", "Fully DLC bundle for Assassin's Creed Valhalla", 120, temp1.getId());
+    public static Dlc dlc3 = new Dlc("Unbound Palace Edition", "Palace extension for Need for Speed™ Unbound", 180, temp3.getId());
 
     public static Publisher publisher1 = new Publisher("Publisher1");
     public static Publisher publisher2 = new Publisher("Publisher2");
@@ -21,7 +21,7 @@ public class TempGame {
         // Add DLCs to game
         temp1.addExtension(dlc1);
         temp1.addExtension(dlc2);
-        temp2.addExtension(dlc3);
+        temp3.addExtension(dlc3);
 
         // Add games to publisher
         publisher1.addGame(temp1);
